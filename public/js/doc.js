506bfb81cfa2b956eb117c71c3f23565186bb0d3
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-12-19 11:47:45 -0800

    Create editor instances

--- public/js/doc.js
+++ public/js/doc.js
@@ -1,3 +1,29 @@
 window.onload = function() {
+	var cssPanel = $('#cssPanel'),
+		htmlPanel = $('#htmlPanel'),
+		javascriptPanel = $('#javascriptPanel');
 
+	var cssEditor = CodeMirror(
+		cssPanel[0],
+		{
+			value: "function myScript(){return 100;}\n",
+			mode:  "css"
+		}
+	);
+
+	var htmlEditor = CodeMirror(
+		htmlPanel[0],
+		{
+			value: "function myScript(){return 100;}\n",
+			mode:  "html"
+		}
+	);
+
+	var javascriptEditor = CodeMirror(
+		javascriptPanel[0],
+		{
+			value: "function myScript(){return 100;}\n",
+			mode:  "javascript"
+		}
+	);
 }
