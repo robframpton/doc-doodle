5c74fd2d828e19906fdc8016b37d701b329caf48
drewbrokke <drew.brokke@liferay.com>
2014-12-19 12:10:55 -0800

    Configure editors

--- public/js/doc.js
+++ public/js/doc.js
@@ -1,29 +1,43 @@
 window.onload = function() {
-	var cssPanel = $('#cssPanel'),
-		htmlPanel = $('#htmlPanel'),
-		javascriptPanel = $('#javascriptPanel');
-
-	var cssEditor = CodeMirror(
-		cssPanel[0],
-		{
-			value: "function myScript(){return 100;}\n",
-			mode:  "css"
-		}
-	);
+	var htmlPanel = $('#htmlPanel'),
+		cssPanel = $('#cssPanel'),
+		javascriptPanel = $('#javascriptPanel'),
+		editors = {};
 
 	var htmlEditor = CodeMirror(
 		htmlPanel[0],
 		{
-			value: "function myScript(){return 100;}\n",
-			mode:  "html"
+			keymap: "sublime",
+			lineNumbers: true,
+			mode:  "html",
+			theme: "ambiance",
+			value: "This is for HTML"
+		}
+	);
+
+	var cssEditor = CodeMirror(
+		cssPanel[0],
+		{
+			keymap: "sublime",
+			lineNumbers: true,
+			mode:  "css",
+			theme: "ambiance",
+			value: "This is for CSS"
 		}
 	);
 
 	var javascriptEditor = CodeMirror(
 		javascriptPanel[0],
 		{
-			value: "function myScript(){return 100;}\n",
-			mode:  "javascript"
+			keymap: "sublime",
+			lineNumbers: true,
+			mode:  "javascript",
+			theme: "ambiance",
+			value: "This is for Javascript"
 		}
 	);
-}
+
+	editors.htmlEditor = htmlEditor;
+	editors.cssEditor = cssEditor;
+	editors.javascriptEditor = javascriptEditor;
+};
